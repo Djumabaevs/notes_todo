5cb52c171aec40f3c2eb97233311b931f2591e53
BakytDzhumabaev <djumabaevb@gmail.com>
2022-01-07 12:09:51 +0600

    first commit

---
+++ lib/models/note_for_listing.dart
@@ -0,0 +1,12 @@
+class NoteForListing {
+  String noteID;
+  String noteTitle;
+  DateTime createDateTime;
+  DateTime latestEditDateTime;
+
+  NoteForListing(
+      {this.noteID,
+      this.noteTitle,
+      this.createDateTime,
+      this.latestEditDateTime});
+}
